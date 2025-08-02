33cdc923611bb78535f673e93eab32a72db47873
Gregory Tomy <prv-GTomy@PV-GTomy.local>
2025-08-02 16:49:28 -0600

    feat: create json data models

--- src/models.rs
+++ src/models.rs
@@ -1,1 +1,6 @@
 mod insured_info;
+mod lab_record;
+mod lifestyle_condition;
+mod medical_record;
+mod rx_record;
+mod test_record;
---
+++ src/models/rx_record.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+use validator::Validate;
+
+#[derive(Debug, Deserialize, Serialize, Validate)]
+pub struct RxRecord {
+    #[validate(length(min = 1))]
+    #[serde(rename = "Impairment")]
+    pub impairment: String,
+
+    #[validate(length(min = 1))]
+    #[serde(rename = "Rx Name")]
+    pub rx_name: String,
+
+    #[validate(length(min = 1))]
+    #[serde(rename = "RX Type")]
+    pub rx_type: String,
+
+    #[serde(rename = "RxNORM")]
+    pub rx_norm: String,
+
+    #[validate(length(min = 1))]
+    #[serde(rename = "Start Date")]
+    pub start_date: String,
+
+    #[serde(rename = "End Date")]
+    pub end_date: Option<String>,
+
+    #[validate(length(min = 1))]
+    #[serde(rename = "Rx Date")]
+    pub rx_date: String,
+
+    #[validate(length(min = 1))]
+    #[serde(rename = "Hyperlink")]
+    pub hyperlink: String,
+
+    #[validate(length(min = 1))]
+    #[serde(rename = "HyperlinkUrl")]
+    pub hyperlink_url: String,
+}
